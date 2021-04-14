20e5f612265752b74effdc88ae75853a31f6c24d
pika96 <52442949+pika96@users.noreply.github.com>
2021-04-14 13:38:15 +0900

    feat: DAO 변경

--- src/main/java/nextstep/helloworld/jdbc/jdbctemplate/QueryingDAO.java
+++ src/main/java/nextstep/helloworld/jdbc/jdbctemplate/QueryingDAO.java
@@ -29,7 +29,8 @@ public class QueryingDAO {
      */
     public int count() {
         String sql = "select count(*) from customers";
-        return 0;
+
+        return this.jdbcTemplate.queryForObject(sql, Integer.class);
     }
 
     /**
@@ -37,7 +38,8 @@ public class QueryingDAO {
      */
     public String getLastName(Long id) {
         String sql = "select last_name from customers where id = ?";
-        return null;
+
+        return this.jdbcTemplate.queryForObject(sql, String.class, id);
     }
 
     /**
@@ -45,7 +47,13 @@ public class QueryingDAO {
      */
     public Customer findCustomerById(Long id) {
         String sql = "select id, first_name, last_name from customers where id = ?";
-        return null;
+
+        return this.jdbcTemplate.queryForObject(sql,
+                (resultSet, rowNum) -> {
+                    Customer customer = new Customer(resultSet.getString("first_name")
+                    , resultSet.getString("last_name"));
+                    return customer;
+                }, id);
     }
 
     /**
@@ -53,7 +61,13 @@ public class QueryingDAO {
      */
     public List<Customer> findAllCustomers() {
         String sql = "select id, first_name, last_name from customers";
-        return null;
+
+        return this.jdbcTemplate.query(sql,
+                (resultSet, rowNum) -> {
+                    Customer customer = new Customer(resultSet.getString("first_name")
+                            , resultSet.getString("last_name"));
+                    return customer;
+                });
     }
 
     /**
@@ -61,6 +75,11 @@ public class QueryingDAO {
      */
     public List<Customer> findCustomerByFirstName(String firstName) {
         String sql = "select id, first_name, last_name from customers where first_name = ?";
-        return null;
+
+        return this.jdbcTemplate.query(sql,(resultSet, rowNum) -> {
+                Customer customer = new Customer(resultSet.getString("first_name")
+                        , resultSet.getString("last_name"));
+                return customer;
+        }, firstName);
     }
 }
